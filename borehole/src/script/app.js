e070667bcb68f7b5ab7ae3f63b0b2b7489ff14c1
bad4iz <bad4iz@gmail.com>
2017-07-27 06:12:32 +0400

    webpack-merge

---
+++ borehole/src/script/app.js
@@ -0,0 +1,5 @@
+import '../sass/test.scss'
+
+let a = 9;
+
+document.write(a);
--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,45 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 
 
 module.exports = {
-  entry: './borehole/src/script/app.js',
-  output: {
-    library: 'Borehole',
-    filename: './borehole/build/script/build.js',
-  },
-
-  watch: true,
-
-  watchOptions: {
-    aggregateTimeout: 100,
-  },
-  plugins: [],
-  devtool: 'source-map',
-
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-        },
-      },
-    ],
-  },
+    entry: './borehole/src/script/app.js',
+    output: {
+        library: 'Borehole',
+        filename: './borehole/build/script/build.js',
+    },
+    
+    watch: true,
+    
+    watchOptions: {
+        aggregateTimeout: 100,
+    },
+    plugins: [],
+    devtool: 'source-map',
+    
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                },
+            },
+        ],
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
+    },
 };
 
 if (NODE_ENV == 'production') {
-  module.exports.plugins.push(
-  );
+    module.exports.plugins.push(
+    );
 }
